a7f9410d6d76c94653c474aa7be784c3911af8ef
Vasavikoyagura <vasavikoyagura33@gmail.com>
2025-04-26 08:08:14 +0000

    font

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent,
+  },
+  {
+    path:'register',
+    component:RegistrationComponent
+  },
+  {
+    path:'',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
