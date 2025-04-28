db868c0061fb8494e16d58821a4d83151cd6c5e4
Dhuhitha <dhuhithabudigem33@gmail.com>
2025-04-28 06:21:24 +0000

    Inital pull

--- angularapp/src/app/components/adminviewfeedback/adminviewfeedback.component.ts
+++ angularapp/src/app/components/adminviewfeedback/adminviewfeedback.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminviewfeedback.component.css']
 })
 export class AdminviewfeedbackComponent implements OnInit {
-
+feedbackText:string='';
   constructor() { }
+submitFeedback(){
+  if(this.feedbackText.trim())
+  {
+    console.log('Feedback submitted:', this.feedbackText);
+    alert('Thank you for your feedback!');
+    this.feedbackText='';
+  }
+  else{
+    console.log('Feedback is required.');
+  }
+}
 
   ngOnInit(): void {
   }
